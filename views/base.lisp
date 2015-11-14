53e2f060ffc3ddb0955937f029663153bc2cfd21
Safia Abdalla <safia@safia.rocks>
2015-11-14 00:38:38 -0600

    Added base template file

---
+++ views/base.lisp
@@ -0,0 +1,10 @@
+(:in-package :papaya)
+
+(deflayout base (data :top-level t)
+    (:html
+      (:head
+        (:title (str (conc (getf data :title) " | Papaya ")))
+        (:link :rel "stylesheet" :href "styles/bootstrap.min.css"))
+      (:body :class (getf data :body-class)
+        (str (getf data :content)))))
+
