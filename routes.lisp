1abdd91c580dc60af22e7006c13b2eeb9488145a
Safia Abdalla <safia@safia.rocks>
2015-11-22 01:47:12 -0600

    Updated routes

--- routes.lisp
+++ routes.lisp
@@ -1,6 +1,16 @@
 (in-package :papaya)
 
-(defroute (:get "/") (request response))
+(defun page-not-found (response)
+  (send-response response
+                 :status 404
+                 :body (layout 'default (list :content "Page not found."
+                                              :title "Page not found."))))
+
+(clear-routes)
+
+(defroute (:get "/") (request response)
+    (let ((body (load-view :content/index)))
+      (send-response response :headers '(:content-type "text/html") :body body)))
 
 (defroute (:get "/audience") (request response))
 
